6b2510d27802d1c2e7e859ca796f7e85e88a48ba
Manzoor Ahmed Wahid <mWahid@justenergy.com>
2019-04-17 08:32:02 -0400

    Initial commit.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { TodoService } from './todo.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  newTodo: string;
+  todos: string[];
+
+  constructor(private todoService: TodoService) { }
+
+  ngOnInit(): void {
+    this.todos = [];
+  }
+
+  addTodo() {
+    if (!this.newTodo) { return; }
+
+    this.todos.push(this.newTodo);
+
+    this.newTodo = '';
+  }
+
+  removeTodo(todo: string) {
+    const i = this.todos.findIndex(t => t === todo);
+
+    if (i !== -1) {
+      this.todos.splice(i, 1);
+    }
+  }
 }
